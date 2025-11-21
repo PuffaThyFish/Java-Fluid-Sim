76489b864b1a36175b8f701add617d89b3a2a28c
Patrick O'Connell <patrickoconnelllll@gmail.com>
2025-11-21 18:32:01 +0000

    workin

---
+++ FluidSim.java
@@ -0,0 +1,21 @@
+public class FluidSim {
+    public static void main(String[] args) {
+        System.out.println("hi ");
+
+        int[][] grid = new int[5][5];
+        System.out.println(grid[0][1]);
+    }
+}
+
+
+class Grid {
+    
+
+    public Grid() {
+        
+    }
+
+    public void printGrid() {
+        System.out.println("hi");
+    } 
+}
