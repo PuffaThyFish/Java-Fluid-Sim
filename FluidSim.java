2ade11243732fe3295a459bc66bd4a05190963e7
PuffaThyFish <dagloriouspuffafish@gmail.com>
2026-03-18 14:18:08 -0400

    more update

--- FluidSim.java
+++ FluidSim.java
@@ -1,6 +1,8 @@
 import java.awt.image.BufferedImage;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.*;
 import javax.swing.*;
 
@@ -39,6 +41,10 @@ class Simulator {
         logicGrid[60][35] = 237;
         panel.updateBuffer(logicGrid);
     }
+
+    public void click(int x, int y) {
+
+    } 
 }
 
 class Panelinator extends JPanel {
@@ -47,20 +53,52 @@ class Panelinator extends JPanel {
     public Panelinator(int width, int height) {
         pixelGrid = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         setPreferredSize(new Dimension(width, height));
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                click(e.getX(), e.getY());
+            }
+        });
     }
 
     public void updateBuffer(int[][] logicGrid) {
         for (int x = 0; x < pixelGrid.getWidth(); x++) {
             for (int y = 0; y < pixelGrid.getHeight(); y++) {
-                pixelGrid.setRGB(x, y, logicGrid[y][x]);
+                pixelGrid.setRGB(x, y, logicGrid[x][y]);
             }
         }
-        this.repaint();
+        repaint();
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(pixelGrid, 0, 0, this);
+    }
+}
+
+class Tile {
+    private int x;
+    private int y;
+
+    public Tile(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getCoords() {
+        return x;
+    }
+}
+
+class Dirt {
+    public int color = 0x0099FF;
+
+    public Dirt(int x, int y) {
+        super()
+    }
+
+    public int moveTile(int[][] logicGrid) {
+        return 
     }
 }
